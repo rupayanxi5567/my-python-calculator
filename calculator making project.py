b49b69d90950ea10b470b9c6a455c0be49ba8c57
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2023-04-22 21:17:53 +0530

    Create calculator making project.py
    my calculator

---
+++ calculator making project.py
@@ -0,0 +1,42 @@
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(8//3)
+print(6%4)
+print(2**3)
+
+
+
+# a = 20
+# b = 10
+# print("the value of", a, "+", b, "is:", a+b)
+# print("the value of",a, "-",b, "is:", a-b  )
+# print("the value of",a, "*",b, "is:", a*b  )
+# print("the value of",a, "/",b, "is:", a/b  )
+
+
+x = float(input("enter first number here:"))
+y = float(input("enter second number here:"))
+
+
+
+
+
+ans1 = x+y
+print("the value of",x, "+", y, "is:", ans1)
+
+ans2 = x-y
+print("the value of",x, "-", y, "is:", ans2)
+
+ans3 = x*y
+print("the value of",x, "*", y, "is:", ans3)
+
+ans4 = x/y
+print("the value of",x, "/", y, "is:", ans4)
+
+ans5 = x//y
+print("the value of",x, "//", y, "is:", ans5 )
+
+ans6 = x**y
+print("the value of",x, "**", y, "is:", ans6)
